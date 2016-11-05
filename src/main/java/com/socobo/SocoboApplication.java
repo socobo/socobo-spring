600bf8366a4a8d6a0c3d19b7e5059da7adff3801
hetec <hebner.tec@gmail.com>
2016-11-05 21:10:09 +0100

    #7 Enable spring security
    - adds basic auth by default to any endpoint
    - encrypt user password during registration

--- src/main/java/com/socobo/SocoboApplication.java
+++ src/main/java/com/socobo/SocoboApplication.java
@@ -3,9 +3,17 @@ package com.socobo;
 import com.socobo.home.HomeController;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
 public class SocoboApplication {
+
+	@Bean
+	public PasswordEncoder passwordEncoder(){
+		return new BCryptPasswordEncoder();
+	}
 
 	public static void main(String[] args) {
 
--- src/main/java/com/socobo/userManagement/service/RegistrationImpl.java
+++ src/main/java/com/socobo/userManagement/service/RegistrationImpl.java
@@ -3,22 +3,33 @@ package com.socobo.userManagement.service;
 import com.socobo.userManagement.model.User;
 import com.socobo.userManagement.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.dao.DataAccessException;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
 
 /**
  * Created by patrick on 05.11.16.
  */
+@Service
 public class RegistrationImpl implements Registration{
 
     private UserRepository userRepository;
+    private PasswordEncoder passwordEncoder;
 
     @Autowired
-    public RegistrationImpl(UserRepository userRepository) {
+    public RegistrationImpl(UserRepository userRepository, PasswordEncoder passwordEncoder) {
         this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
     }
 
     @Override
     public User register(User user) {
-        return userRepository.save(user);
+        return userRepository.save(getEncryptedPasswordUser(user));
+    }
+
+    private User getEncryptedPasswordUser(User user) {
+        return new User(
+                    user.getUsername(),
+                    user.getEmail(),
+                    passwordEncoder.encode(user.getPassword()));
     }
 }
